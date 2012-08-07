2ced9efb9df5ffd7513bdf1179207119868ff2bc
lh2 <lh2@hushmail.com>
2012-08-07 14:36:44 +0200

    Mark interval box if value is invalid

--- main.js
+++ main.js
@@ -191,28 +191,37 @@
         $('#submit').click(function() {
             var widthBox = $('#gridWidth');
             var heightBox = $('#gridHeight');
+            var intervalBox = $('#gridInterval');
             
             var width = widthBox.val();
             var height = heightBox.val();
+            var intervalValue = intervalBox.val();
             
             var markWidthBox = !width.match(/^[0-9]+$/);
             var markHeightBox = !height.match(/^[0-9]+$/);
+            var markIntervalBox = !intervalValue.match(/^[0-9]+$/);
             
             if(markWidthBox)
                 widthBox.css("background-color", "#FF0000");
             else
                 widthBox.css("background-color", "");
-
+            
             if(markHeightBox)
                 heightBox.css("background-color", "#FF0000");
             else
                 heightBox.css("background-color", "");
             
-            if(!markWidthBox && !markHeightBox)
+            if(markIntervalBox)
+                intervalBox.css("background-color", "#FF0000");
+            else
+                intervalBox.css("background-color", "");
+            
+            if(!markWidthBox && !markHeightBox && !markIntervalBox)
             {
                 newGrid(width, height);
                 widthBox.css("background-color", "");
                 heightBox.css("background-color", "");
+                intervalBox.css("background-color", "");
             }
         });
     });
